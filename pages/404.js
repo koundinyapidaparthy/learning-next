278c25dd962bb748326c95f12cc02e3f9b3158f2
Koundinya Pidaparthy <koundinya.pidaparthy@cityexperiences.com>
2023-06-16 16:59:15 +0530

    Completed with section 3

---
+++ pages/404.js
@@ -0,0 +1,4 @@
+function NotFoundPage() {
+  return <div>404 Page Not Found</div>;
+}
+export default NotFoundPage;
--- pages/index.js
+++ pages/index.js
@@ -1,3 +1,45 @@
+import Link from "next/link";
 export default function Home() {
-  return <div>Hello</div>;
+  const clientsData = [
+    {
+      id: "max",
+      name: "MaxStar",
+      subname: "client1",
+    },
+    {
+      id: "gopal",
+      name: "Gopal",
+      subname: "client1",
+    },
+    { id: "mister", name: "Mister", subname: "client1" },
+  ];
+  return (
+    <div>
+      <ul>
+        <li>
+          <Link href="/products">Products</Link>
+        </li>
+        <li>
+          <Link href="/about">About</Link>
+        </li>
+        {clientsData.map(({ id, name, subname }) => {
+          return (
+            <li key={id}>
+              <Link
+                href={{
+                  pathname: "/clients/[id]/[clientproject]",
+                  query: {
+                    id: id,
+                    clientproject: subname || "",
+                  },
+                }}
+              >
+                {name}
+              </Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
+  );
 }
